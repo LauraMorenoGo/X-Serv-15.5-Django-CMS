d7a1dcc184d1317e0c161b14338b5ff9979bdc86
Laura <laura.mo.go@hotmail.com>
2018-04-04 14:33:19 +0200

    Entrega ejercicio

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from cms import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    
+    url(r'^$', views.listar, name="Direcciones base de datos"),
+    url(r'(.+)', views.buscar, name="Página"),
     url(r'^admin/', include(admin.site.urls)),
 )
